37541510a51149c6b8477ff97206e938530e45a0
Gavin Stark <gstark31897@gmail.com>
2017-04-29 14:07:33 +0000

    adding constructor

--- include/fileobject.h
+++ include/fileobject.h
@@ -10,6 +10,8 @@ private:
     std::string m_name;
 
 public:
+    FileObject(std::string name): m_name(name) {};
+
     std::string getName() { return m_name; };
 
     bool operator<(FileObject &other) { return m_name < other.getName(); };
